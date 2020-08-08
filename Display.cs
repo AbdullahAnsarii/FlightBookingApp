b64b3b529640e37b414e8fd41e889dbc49d624ef
Abdullah Ansari <aa9272137@gmail.com>
2020-08-09 03:27:45 +0500

    completed

---
+++ Display.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace flightBookingApp
+{
+    public partial class Display : Form
+    {
+        public Display()
+        {
+            InitializeComponent();
+        }
+
+        private void Display_Load(object sender, EventArgs e)
+        {
+            if (Form1.IDCard)
+            {
+                lbldocumentno.Text = "ID Card No.";
+            }
+            else
+            {
+                lbldocumentno.Text = "Passport No.";
+            }
+
+            fname.Text = Form1.firstName;
+            lname.Text = Form1.lastName;
+            goingto.Text = Form1.countryTo;
+            goingfrom.Text = Form1.countryFrom;
+            documentno.Text = Form1.documentNo;
+            docissuedate.Text = Form1.docIssuedate;
+            docexpirydate.Text = Form1.docExpirydate;
+            tripstartdate.Text = Form1.tripStartdate;
+            tripenddate.Text = Form1.tripEnddate;
+            valuebaggage.Text = Form1.baggage;
+            datetimereciept.Text = Form1.formattedDate;
+            
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace flightBookingApp
+{
+    public partial class Form1 : Form
+    {
+        public static bool Passport, IDCard;
+        public static string countryTo;
+        public static string countryFrom;
+        public static string firstName;
+        public static string lastName;
+        public static string documentNo;
+        public static string tripStartdate;
+        public static string tripEnddate;
+        public static string docIssuedate;
+        public static string docExpirydate;
+        public static string baggage;
+        public static string formattedDate;
+
+        Display dis = new Display();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime myDate = DateTime.Now;
+            formattedDate = string.Format("{0:dd/MM/yy HH:mm:ss:tt}", myDate);
+            dateOpened.Text = formattedDate;
+            
+            
+            calenderMonth.MaxSelectionCount = 100;
+            calenderMonth.ShowToday = true;
+        }
+
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbIdcard.Checked)
+            {
+                lblDocno.Text = "ID No.";
+                lblDocissuedate.Text = "ID Card Issue Date.";
+                lblDocexpirydate.Text = "ID Card Expiry Date";
+                IDCard = true;
+            }
+        }
+
+        private void datePick1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime dt = datePick1.Value;
+            datePick2.MinDate = dt;
+        }
+
+        private void btnBook_Click(object sender, EventArgs e)
+        {
+            firstName = boxFirstname.Text;
+            countryTo = boxTo.Text;
+            countryFrom = boxFrom.Text;
+            lastName = boxLastname.Text;
+            documentNo = boxDocno.Text;
+            docIssuedate = datePick1.Value.ToString("dd/MM/yy");
+            docExpirydate = datePick2.Value.ToString("dd/MM/yy");
+            tripStartdate = calenderMonth.SelectionStart.ToString("dd MMMM yyyy");
+            tripEnddate = calenderMonth.SelectionEnd.ToString("dd MMMM yyyy");
+            baggage = comboBox.Value.ToString();
+            
+            
+            this.Hide();
+            dis.Show();
+
+        }
+        private void comboBox_ValueChanged(object sender, EventArgs e)
+        {
+            //comboBox.Value=0;
+            comboBox.Increment = 5;
+        }
+
+       
+
+        private void rbPassport_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbPassport.Checked)
+            {
+                lblDocno.Text = "Passport No.";
+                lblDocissuedate.Text = "Passport Issue Date.";
+                lblDocexpirydate.Text = "Passport Expiry Date";
+                Passport = true;
+            }
+        }
+    }
+}
